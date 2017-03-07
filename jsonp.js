ed43e83e3eede78d0541a9b1a348d9d830468341
Kyle H <k-yle@users.noreply.github.com>
2017-03-08 08:35:42 +1300

    Update jsonp.js

--- jsonp.js
+++ jsonp.js
@@ -1,7 +1,11 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) 
-    AKLBusDelayData(JSON.parse(this.responseText));
-};
-xhr.open('GET', 'https://k-yle.github.io/AKL-Bus-Delays/database.json', true);
-xhr.send();
+function XHR(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) 
+      cb(JSON.parse(this.responseText));
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+XHR('https://k-yle.github.io/AKL-Bus-Delays/database.json', AKLBusDelayData);
+XHR('https://k-yle.github.io/AKL-Bus-Delays/stopsManifest.json', AKLBusDelayManifest);
